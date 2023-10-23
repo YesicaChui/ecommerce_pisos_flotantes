16cb4f27d8677208b61775d727676d7624f39969
YesicaChui <yesicachuic@gmail.com>
2023-10-23 01:48:01 -0500

    footer de autoria

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer >
+      <p>
+        <a target="_blank" href="https://www.linkedin.com/in/yesicachuichalco/" >Yesica Chui Chalco</a>
+        © 2023 - <strong> Todos los derechos reservados </strong></p>
+      <p >Este es un proyecto Freelance desarrollado para el ECOMMERCE NGL</p>
+    </footer>
+  )
+}
--- src/layouts/LayoutBase.jsx
+++ src/layouts/LayoutBase.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
 
 
 export const LayoutBase = ({children}) => {
@@ -7,6 +8,7 @@ export const LayoutBase = ({children}) => {
     <>    
     <Header/>
     {children}
+    <Footer/>
     </>
   )
 }
