37b3220afffb5f87ff8c34465f1e627e5da2c841
YesicaChui <yesicachuic@gmail.com>
2023-10-23 01:59:56 -0500

    retoques 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ export const Footer = () => {
     <footer >
       <p>
         <a target="_blank" href="https://www.linkedin.com/in/yesicachuichalco/" >Yesica Chui Chalco</a>
-        © 2023 - <strong> Todos los derechos reservados </strong></p>
-      <p >Este es un proyecto Freelance desarrollado para el ECOMMERCE NGL</p>
+        © 2023 - <strong> Todos los derechos reservados</strong></p>
+      <p >Este es un proyecto Freelance desarrollado para ECOMMERCE NGL</p>
     </footer>
   )
 }
